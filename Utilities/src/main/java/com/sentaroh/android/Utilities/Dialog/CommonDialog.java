92f2bb472abf324f3afdaedda3f3f35284dfbaaf
PhilZ <phytowardt@gmail.com>
2020-09-27 16:53:03 +0200

    Add selective opacity depending on toasts types
    This matches the Android system global notiifcations transparency

--- Utilities/src/main/java/com/sentaroh/android/Utilities/Dialog/CommonDialog.java
+++ Utilities/src/main/java/com/sentaroh/android/Utilities/Dialog/CommonDialog.java
@@ -91,14 +91,17 @@ public class CommonDialog {
     static private final int TOAST_MESSAGE_FG_COLOR_LIGHT=Color.WHITE;
     static private final int TOAST_MESSAGE_BG_COLOR=Color.LTGRAY;
     static private final int TOAST_MESSAGE_BG_COLOR_LIGHT=0xff666666;
-    static private final float TOAST_MESSAGE_TRANSLUCENT_RATIO =1.0f;
+    static private final float TOAST_MESSAGE_OPACITY_100=1.0f;
+    static private final float TOAST_MESSAGE_OPACITY_80=0.8f;
+
+    //mainly used for App system notifications (Autosave settings, Sync task started...)
     static private Toast showToast(Activity a, String msg, int duration) {
         Toast toast=Toast.makeText(a, msg, duration);
         View tv=toast.getView();
-        int fg_color= TOAST_MESSAGE_FG_COLOR, bg_color=getColorWithAlpha(TOAST_MESSAGE_BG_COLOR, TOAST_MESSAGE_TRANSLUCENT_RATIO);
+        int fg_color= TOAST_MESSAGE_FG_COLOR, bg_color=getColorWithAlpha(TOAST_MESSAGE_BG_COLOR, TOAST_MESSAGE_OPACITY_80);
         if (ThemeUtil.isLightThemeUsed(a)) {
             fg_color=TOAST_MESSAGE_FG_COLOR_LIGHT;
-            bg_color=getColorWithAlpha(TOAST_MESSAGE_BG_COLOR_LIGHT, TOAST_MESSAGE_TRANSLUCENT_RATIO);//<-Color.DKGRAY 0xff444444;
+            bg_color=getColorWithAlpha(TOAST_MESSAGE_BG_COLOR_LIGHT, TOAST_MESSAGE_OPACITY_80);//<-Color.DKGRAY 0xff444444;
         }
 
         GradientDrawable drawable = new GradientDrawable();
@@ -175,11 +178,12 @@ public class CommonDialog {
 
     }
 
+    //mainly used for context buttons notification info
     static private void setToastMessageView(Activity a, View custom_toast_view, TextView toast_message) {
-        int fg_color= TOAST_MESSAGE_FG_COLOR, bg_color=getColorWithAlpha(TOAST_MESSAGE_BG_COLOR, TOAST_MESSAGE_TRANSLUCENT_RATIO);
+        int fg_color= TOAST_MESSAGE_FG_COLOR, bg_color=getColorWithAlpha(TOAST_MESSAGE_BG_COLOR, TOAST_MESSAGE_OPACITY_100);
         if (ThemeUtil.isLightThemeUsed(a)) {
             fg_color=TOAST_MESSAGE_FG_COLOR_LIGHT;
-            bg_color=getColorWithAlpha(TOAST_MESSAGE_BG_COLOR_LIGHT, TOAST_MESSAGE_TRANSLUCENT_RATIO);//<-Color.DKGRAY 0xff444444;
+            bg_color=getColorWithAlpha(TOAST_MESSAGE_BG_COLOR_LIGHT, TOAST_MESSAGE_OPACITY_100);//<-Color.DKGRAY 0xff444444;
         }
 
         toast_message.setTextColor(fg_color);
